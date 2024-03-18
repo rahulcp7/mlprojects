f750d22ec09d92eb7023974775a792185d3aa683
Rahul C P <rahulcp91@gmail.com>
2024-03-18 22:30:45 +0530

    Adding Jupyter notebook for EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import sys
+import sys 
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb= error_detail.exc_info()
@@ -10,10 +10,11 @@ def error_message_details(error,error_detail:sys):
         str(error )) 
     return error_message 
     
-class customException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message ,error_detail:sys):
         super.__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
